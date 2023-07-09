1d48e1454d68a4b885dfb5fd528bd60a115fda75
andrey <andreyhideki@gmail.com>
2023-07-09 04:56:11 -0300

    estudando migration

--- database/migrations/2023_07_05_014732_create_products_table.php
+++ database/migrations/2023_07_05_014732_create_products_table.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Query\Expression;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
@@ -17,7 +18,26 @@ return new class extends Migration
             $table->string('nome');
             $table->decimal('preco');
             $table->integer('quantidade');
+            $table->enum('difficulty', ['tipo 1', 'tipo 2', 'tipo 3'])->comment('difficulty: tipo 1, tipo 2, tipo 3');
+            $table->jsonb('options')->nullable();
+            //$table->jsonb('optionsdois')->default(new Expression('(JSON_ARRAY())'));
+            $table->longText('description')->nullable();
+
+            // $table->unsignedBigInteger('user_id');
+            // $table->foreign('user_id')->references('id')->on('users');
+            $table->foreignId('user_id')
+                ->constrained(
+                    table: 'users', indexName: 'produtos_user_id'
+                )
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
         });
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('name', 100)->change();
+        });
+
+
     }
 
     /**
