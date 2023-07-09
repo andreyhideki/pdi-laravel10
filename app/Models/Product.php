c5dcf38136be15759e23a649797a3ec3e0f7b91e
andrey <andreyhideki@gmail.com>
2023-07-09 04:28:14 -0300

    feat: adicionando propriedades na classe product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,10 +2,61 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
     use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'products';
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'product_id';
+
+    /**
+     * Indicates if the model's ID is auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The data type of the auto-incrementing ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /**
+     * @var string
+     */
+    public $nome;
+
+    /**
+     * @var decimal
+     */
+    public $preco;
+
+    /**
+     * @var integer
+     */
+    public $quantidade;
+
+    public function setNome($value)
+    {
+        $this->attributes['nome'] = $value;
+    }
+
+    
 }
--- database/migrations/2023_07_05_014732_create_products_table.php
+++ database/migrations/2023_07_05_014732_create_products_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('nome');
+            $table->decimal('preco');
+            $table->integer('quantidade');
         });
     }
 
